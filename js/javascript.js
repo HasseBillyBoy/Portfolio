55b5d93f27e55783853101283e2430547f53e155
Hasse <haje0001@stud.kea.dk>
2023-06-08 20:12:56 +0200

    ok, test

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+window.onload = function (){
+
+    const menu_btn = document.querySelector('.hamburger');
+    const mobil_menu = document.querySelector('.mobil-nav');
+
+    menu_btn.addEventListener('click', function () {
+
+    menu_btn.classList.toggle('is-active');
+    mobil_menu.classList.toggle('is-active');
+
+});
+
+}
+
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var container = document.getElementById("myHeader");
